652abda60ac1d1209a3dc7ece43ee1c057cc7242
Giga Khizanishvili <gkhizanishvili@adjarabet.com>
2022-12-23 12:18:59 +0400

    Initial Commit

---
+++ Good Food Geo/Good_Food_GeoApp.swift
@@ -0,0 +1,20 @@
+//
+//  Good_Food_GeoApp.swift
+//  Good Food Geo
+//
+//  Created by Giga Khizanishvili on 23.12.22.
+//
+
+import SwiftUI
+
+@main
+struct Good_Food_GeoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
