325076faaf17175b70c946433dc506a083383140
Giga Khizanishvili <khizanag@gmail.com>
2023-01-16 22:12:00 +0400

    Finish donation page

--- Good Food Geo/Presentation/Donation/DonationView.swift
+++ Good Food Geo/Presentation/Donation/DonationView.swift
@@ -15,28 +15,65 @@ struct DonationView: View {
         (Company.liberty, "GE54TB7337945064300021")
     ]
 
+    private let receiver = "ა(ა)იპ საქართველოს ტრენინგებისა და განვითარების ინსტიტუტი"
+    private let purpose = "დონაცია/ქველმოქმედება"
+
     @State private var isSharePresented = false
 
     // MARK: - Body
     var body: some View {
-        StaticPage(
-            section: .init(title: Localization.aboutDonationSectionTitle(), description: Localization.aboutDonationSectionDescription()),
-            subSections: [
-                .init(title: Localization.donationBankAccountNumber(), content: {
-                    AnyView(
-                        VStack {
-                            ForEach(banks, id: \.0.name) { bank, accountNumber in
-                                BankButton(company: bank, accountNumber: accountNumber) {
-                                    isSharePresented = true
+        ScrollView {
+            VStack {
+                StaticPage(
+                    section: .init(title: Localization.aboutDonationSectionTitle(), description: Localization.aboutDonationSectionDescription()),
+                    subSections: [
+                        .init(title: Localization.donationBankAccountNumber(), content: {
+                            VStack {
+                                ForEach(banks, id: \.0.name) { bank, accountNumber in
+                                    BankButton(company: bank, accountNumber: accountNumber) {
+                                        isSharePresented = true
+                                    }
+                                    .sheet(isPresented: $isSharePresented, content: {
+                                        ActivityViewController(activityItems: [accountNumber])
+                                    })
                                 }
-                                .sheet(isPresented: $isSharePresented, content: {
-                                    ActivityViewController(activityItems: [accountNumber])
-                                })
                             }
-                        }
-                    )
-                })
-            ])
+                            .toAnyView()
+                        }),
+                        .init(title: "მიმღები", content: {
+                            Menu {
+                                Button(action: {
+                                    UIPasteboard.general.string = receiver
+                                }, label: {
+                                    Label("Copy", systemImage: "doc.on.doc.fill")
+                                })
+                            } label: {
+                                Text(receiver)
+                                    .multilineTextAlignment(.leading)
+                                    .foregroundColor(.black)
+                            }
+                            .toAnyView()
+                        }),
+                        .init(title: "დანიშნულება", content: {
+                            Menu {
+                                Button(action: {
+                                    UIPasteboard.general.string = purpose
+                                }, label: {
+                                    Label("Copy", systemImage: "doc.on.doc.fill")
+                                })
+                            } label: {
+                                Text(purpose)
+                                    .multilineTextAlignment(.leading)
+                                    .foregroundColor(.black)
+                            }
+                            .toAnyView()
+                        })
+                    ]
+                )
+
+                VSpacing(16)
+            }
+        }
     }
 }
 
